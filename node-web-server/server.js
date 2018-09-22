3263d46f96098fc71049b6f8a087736ba4781ee2
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2018-09-22 15:32:17 -0400

    added handlebars template

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    // res.send('hello express!');
-    res.send({
-        name: 'Tomo',
-        likes: [
-            "Game",
-            "Tennis"
-        ]
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
     })
 });
 
 app.get('/about', (req, res) => {
-    res.send('about page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
